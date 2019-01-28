490b8b381ecf6d0690ecbafab02ebe221ead0f6c
lucyholic <lucy.j.kim@gmail.com>
2019-01-27 20:21:55 -0500

    edited db connection

--- config/config.php
+++ config/config.php
@@ -1,11 +1,8 @@
 <?php
 	$config = array(
-		"host"=>"localhost",
-		//"duser"=>"id7523932_master",
-		//"dpw"=>"abcd123",
-		//"dname"=>"id7523932_lucidkart"
-		"duser"=>"root",
-		"dpw"=>"lucy1213",
+		"host"=>"lucidkart.ctpaoqmnzm15.us-east-2.rds.amazonaws.com",
+		"duser"=>"admin",
+		"dpw"=>"admin123",
 		"dname"=>"lucidkart"
 	);
 ?>
