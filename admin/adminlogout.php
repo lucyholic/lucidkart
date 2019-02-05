13f5edc4458948c2e21fbc45c53f7a8a186ce894
lucyholic <lucy.j.kim@gmail.com>
2019-02-04 22:47:23 -0500

    Edit minor error on admin login

--- admin/adminlogout.php
+++ admin/adminlogout.php
@@ -6,7 +6,7 @@ unset admin session and redirect to the site home
 
 	require_once('lib/header.php');		
     
-    if (!isset($_SESSION['admin']))
+    if (isset($_SESSION['admin']))
     {
         unset($_SESSION['admin']);
     }
